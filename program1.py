976a420374971fde6aa853db6471bbd16771d28e
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:27:41 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -12,7 +12,8 @@ class Solution:
                     if r<0 or r>=rows or c<0 or c>=cols or grid[i][j]=='W':
                         return
                     grid[i][j] = 'W'
-                    dfs(i, j)
+                    dfs(r-1,c)
+                    dfs
 
         
                     
