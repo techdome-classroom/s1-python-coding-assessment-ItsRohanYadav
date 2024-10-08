ab59aae13a340e1af6106e7a45dd00f5976785d9
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:29:31 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,8 @@ class Solution:
         total_isles=0
         def dfs(i,j):
             if i<0 or i>=rows or j<0 or j>=cols or grid[i][j]=='W':
-                        
+                        return
+            
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 'L':
