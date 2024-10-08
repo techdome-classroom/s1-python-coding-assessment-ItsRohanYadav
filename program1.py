81a2a2cc136d37c3342bb7b4fb259c21b8221898
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:10:44 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,7 @@ class Solution:
     #    write your code here
         rows, cols =len(grid), len(grid[0])
         total_isles=0
-        
+        fo
         
                     
         return 0 
