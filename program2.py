e2a886435a09f4001c2444bb22c141e2d17b760a
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:38:35 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,5 +11,6 @@ def decode_message( s: str, p: str) -> bool:
                 elif k<len(p) and p[k]=='*':
                         star_idx = k
                         k+=1
-                elif star
+                elif star_idx != -1:
+                        
         return False
