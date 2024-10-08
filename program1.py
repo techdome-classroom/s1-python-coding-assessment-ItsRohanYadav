b53fb52a692c462d804e649b14178dad40c9c8a9
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:27:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -9,11 +9,6 @@ class Solution:
                 if grid[i][j] == 'L':
                     total_isles += 1
                     dfs(i, j)
-                    if r<0 or r>=rows or c<0 or c>=cols or grid[i][j]=='W':
-                        return
-                    grid[i][j] = 'W'
-                    dfs(r-1,c)
-                    dfs
 
         
                     
