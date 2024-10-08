2d03ba933133fdc94b764547d7a167371c4d8577
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:40:43 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -18,5 +18,5 @@ def decode_message( s: str, p: str) -> bool:
                         m=match
                 else :
                         return False
-        
+        while k
         return False
