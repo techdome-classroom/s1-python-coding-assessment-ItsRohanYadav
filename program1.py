d3943ff6f448160c9201317c67e901fafd46c257
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-10-08 12:28:31 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,12 @@ class Solution:
     #    write your code here
         rows, cols =len(grid), len(grid[0])
         total_isles=0
+        def dfs(i,j):
+            if r<0 or r>=rows or c<0 or c>=cols or grid[i][j]=='W':
+                        return
+                    grid[i][j] = 'W'
+                    dfs(r-1,c)
+                    dfs
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 'L':
